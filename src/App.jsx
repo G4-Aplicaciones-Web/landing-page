19d778f36c50c5149c1b2852c8dd32e5316bfa59
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-04-27 01:17:29 -0500

    feat: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Register from './components/Register';
+import Login from './components/Login';
+import NavBar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      colors: {
+        "sky-green": "#CDFFD6",
+        "dark-green": "#63B663",
+        "soft-gray": "#616060",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
